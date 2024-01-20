bf2c67eeb8c8ec7d3c099af9d48b9907bba31187
Annikaaai <test@ya.ru>
2024-01-20 23:01:44 +0300

    push

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-container>
+      <v-row>
+
+           <v-col ><!-- v-for="(item, i) in news" v-bind:key="i" -->
+            <v-card>
+              <v-img>
+                <v-card-title></v-card-title>
+              </v-img>
+
+              <v-card-text>
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio dolorum illum accusamus eos nam atque placeat incidunt, repellendus facere tempora ratione omnis eius minus, quis autem beatae! Quis, reprehenderit aliquid distinctio corporis molestias excepturi! Maxime, nobis numquam. Similique, reiciendis tenetur voluptate sunt voluptatem assumenda eius asperiores, ipsum, eligendi sint perferendis.
+                </p>
+              </v-card-text>
+            </v-card>
+          </v-col>
+
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+  },
+
+  data: () => ({
+    news: []
+  }),
+  methods: () => ({
+    // getNews() {
+    //   this.axios.get('https://newsapi.org/v2/top-headlines?country=ru&apiKey=d7f41a32c26b4bbfb596d58b1a54c766&roistat_visit=3523472')       
+    //   .then((response) => {    
+    //     this.news = response.data.articles;
+    //     console.log(this.news[0])
+    //   })   
+    // },
+
+    // getNews() {
+    //   this.axios.get('https://newsapi.org/v2/top-headlines?country=ru&apiKey=d7f41a32c26b4bbfb596d58b1a54c766&roistat_visit=3523472')       
+    //   .then((response) => {    
+    //     console.log(1)     
+        
+    //     this.news = response.data.articles
+    //     console.log(this.news[0].title) 
+    //   })   
+    // },
+
+
+    getNews(){
+      this.axios({
+      method: "GET",
+      url: 'https://newsapi.org/v2/top-headlines?country=us8apiKey-d7f41a32c26b4bbfb596d58b1a54c766',
+      }).then( (response) =>{
+        this.news = response.data.articles;
+      })
+    }
+    
+  }),
+  mounted(){
+    this.getNews();
+  }
+};
+</script>
